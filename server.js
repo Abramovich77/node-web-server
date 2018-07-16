894a00405b474667e828f6cf6abd89a31586fb87
Oleg <nicahon@gmail.com>
2018-07-16 17:06:50 +0300

    Test

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 
-
+/*
 app.use((req,res,next) =>{
     res.render('middeware.hbs');
 
 });
-
+*/
 
 app.use(express.static(__dirname + '/public'));
 
